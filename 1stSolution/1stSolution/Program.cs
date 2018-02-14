13dac94b92e9d4f43290f26dca151a04dfd95330
Ibrahim Ahmed <ibrahimkamal77@gmail.com>
2018-02-13 19:23:55 -0500

    Lab 1
    Lab 1

---
+++ 1stSolution/1stSolution/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace stSolution
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            System.Console.Write("First Name: ");
+            string firstName = Console.ReadLine();
+            Console.Write("Middle Initial: ");
+            string middleInitial = Console.ReadLine();
+            Console.Write("last Name: ");
+            string lastName = Console.ReadLine();
+
+            string fullName = firstName + " " + middleInitial + " " + lastName;
+
+            Console.Write("Height in feet: ");
+            int heightFeet = int.Parse(Console.ReadLine());
+            Console.Write("Height in Inches: ");
+            double heightInches = double.Parse(Console.ReadLine());
+
+            double heightCentimeters = (heightFeet * 12 + heightInches) * 2.54;
+
+            Console.Write("Age: ");
+            int age = int.Parse(Console.ReadLine());
+            Console.Write("Citizen? (y/n) : ");
+            bool isCitiznen = Console.ReadLine().ToLower().StartsWith("y");
+
+            bool canVote = isCitiznen && age >= 18;
+
+            Console.WriteLine(fullName);
+            Console.WriteLine(heightCentimeters);
+            Console.WriteLine(canVote);
+        }
+    }
+}
