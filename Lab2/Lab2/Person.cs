7ca2067cad2fc6a4e7caad508e00bd2cdd91f778
Ibrahim Ahmed <ibrahimkamal77@gmail.com>
2018-05-08 04:22:10 -0400

    Project 2
    This is Project 2

---
+++ Lab2/Lab2/Person.cs
@@ -0,0 +1,61 @@
+﻿namespace Lab2
+{
+    class Person
+    {
+        public int Age;
+        public string FirstName;
+        public string LastName;
+        public Person Spouse;
+
+        public static double SumOfAllAges;
+        public static int Counter;
+
+        public static double AverageAge()
+        {
+            return Person.SumOfAllAges / Person.Counter;
+        }
+
+        public void PersonInfo()
+        {
+            System.Console.Write("Enter Your First Name:   ");
+            FirstName = System.Console.ReadLine();
+
+            System.Console.Write("Enter Your Last Name:    ");
+            LastName = System.Console.ReadLine();
+
+            System.Console.Write("Enter Your Age:          ");
+            Age = int.Parse(System.Console.ReadLine());
+
+            Person.Counter++;
+            Person.SumOfAllAges += Age;
+        }
+
+        public void SpouseInfo()
+        {
+            System.Console.Write("Spouse's First Name:     ");
+            Spouse.FirstName = System.Console.ReadLine();
+
+            System.Console.Write("Spouse's Age:            ");
+            Spouse.Age = int.Parse(System.Console.ReadLine());
+
+            Person.Counter++;
+            Person.SumOfAllAges += Spouse.Age;
+        }
+
+        public string GetFullName()
+        {
+            return LastName + ", " + FirstName;
+        }
+
+        public string GetSpouseFullName()
+        {
+            return Spouse.FirstName + ", " + this.LastName;
+        }
+
+        public void PrintNameAndAge()
+        {
+            System.Console.WriteLine(GetFullName() + "  " + "(" + this.Age + ")");
+            System.Console.WriteLine(GetSpouseFullName() + "  " + "(" + this.Spouse.Age + ")");
+        }
+    }
+}
---
+++ Lab2/Lab2/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p1 = new Person();
+            p1.Spouse = new Person();
+
+            Person p2 = new Person();
+            p2.Spouse = new Person();
+
+            System.Console.WriteLine("[ Enter Information for p1 ]");
+            System.Console.WriteLine("");
+
+            p1.PersonInfo();
+            p1.SpouseInfo();
+
+            System.Console.WriteLine("");
+            System.Console.WriteLine("[ Enter Information for p2 ]");
+            System.Console.WriteLine("");
+
+            p2.PersonInfo();
+            p2.SpouseInfo();
+
+            System.Console.WriteLine("");
+            System.Console.WriteLine("[ Printing Results ]");
+            System.Console.WriteLine("");
+
+            p1.PrintNameAndAge();
+            p2.PrintNameAndAge();
+
+            System.Console.WriteLine("");
+            System.Console.WriteLine("Average Age = " + Person.AverageAge());
+            System.Console.Read();
+        }
+    }
+}
